e21c8c88d8bee809f0902d00c139669e19775072
Yilmaz Yagiz Dokumaci <dokumaciyagiz@gmail.com>
2022-12-30 22:07:20 +0300

    First ios app initialized

--- MyFirstApp/ViewController.swift
+++ MyFirstApp/ViewController.swift
@@ -8,12 +8,21 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var viewImage: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+  
+    @IBAction func changeClicked(_ sender: Any) {
+        if (viewImage.image == UIImage(named: "acdc1")) {
+            viewImage.image = UIImage(named: "acdc2");
+        } else {
+            viewImage.image = UIImage(named: "acdc1");
+        }
+        
+    }
 }
 
